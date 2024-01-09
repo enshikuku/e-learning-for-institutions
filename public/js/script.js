9053315f6d938612afe64878c7fe83ee585d5795
Shikuku39 <enshikuku39@gmail.com>
2024-01-09 16:13:08 +0300

    removed scroll to bottom btn

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,6 @@
 const load = document.querySelector(".loader")
 let students = document.querySelectorAll('.studentidandname')
 let details = document.querySelectorAll('.studentdetailedinformation')
-var button = document.querySelector('#scroll-to-bottom-button')
 
 window.addEventListener("load", function(){
     load.style.display = 'none'
@@ -13,33 +12,9 @@ students.forEach(student => {
         if (detail.style.display === 'none') {
             details.forEach(detail => detail.style.display = 'none')
             detail.style.display = 'flex'
-            
         } else {
             detail.style.display = 'none'
         }
     })
 })
-button.addEventListener("click", function() {
-    var currentHeight = document.body.scrollHeight
-    window.scrollTo(0, currentHeight)
-})
 
-var button = document.getElementById("scroll-to-bottom-button")
-button.addEventListener("click", function() {
-    window.scroll({
-        top: document.body.scrollHeight,
-        left: 0,
-        behavior: 'smooth'
-    })
-})
-
-function showHideButton() {
-    var currentHeight = document.body.scrollHeight
-    if (currentHeight > window.innerHeight) {
-        button.style.display = "block"
-    } else {
-        button.style.display = "none"
-    }
-}
-showHideButton()
-window.addEventListener("resize", showHideButton)
