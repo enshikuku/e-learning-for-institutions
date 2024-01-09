2e56ac52ba811e6117b0d315f67d8e2b4563c282
Shikuku39 <enshikuku39@gmail.com>
2024-01-09 12:09:10 +0300

    unused files

--- public/main.js
+++
@@ -1,9 +0,0 @@
-const load = document.querySelector('.loadermain')
-
-window.addEventListener("load", function(){
-    load.style.display = 'none'
-})
-
-function closeNav() {
-    document.querySelector('#nav-check').checked = false
-}
